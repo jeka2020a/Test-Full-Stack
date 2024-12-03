ff9c366fe20f4e833c139ea97742c616f68e82b6
jeka2020a <evgenzekul@gmail.com>
2024-12-03 15:27:10 +0200

    add DataTable

--- mindconnect.ua/script/api.js
+++ mindconnect.ua/script/api.js
@@ -1,57 +1,22 @@
 $(document).ready(function () {
 
-    let table = new DataTable('#myTable');
-
-
-    $('#myTable').DataTable();
-    
-    
-    
-    
-    let page = 1; 
-
-    
-    function loadData(page) {
-        $.get(`http://api.mindconnect.ua/posts/${page}`, function (result) {
-            $('.tbody').empty(); 
-            result.forEach(element => {
-                $('.tbody').append(`
-                    <tr>
-                        <td>${element.id}</td>
-                        <td>${element.username}</td>
-                        <td>${element.number}</td>
-                        <td>${element.fibbonachi}</td>
-                        <td>${element.ip}</td>
-                    </tr>
-                `);
-            });
-        }).fail(function () {
-            console.error("Ошибка загрузки данных");
-        });
-    }
-
-    
-
-
-
-    
-    $('.prev').on('click', function () {
-        if (page > 1) { 
-            page--;
-            loadData(page); 
-        }
+    $('#myTable').DataTable({
+        ajax: {
+            url: 'http://api.mindconnect.ua/all/', 
+            type: 'GET',                        
+            dataSrc: '',                         
+            dataType: 'json'
+        },
+        columns: [
+            { data: 'id' },
+            { data: 'username' },
+            { data: 'number' },
+            { data: 'fibbonachi' },
+            { data: 'ip' }
+        ]
     });
 
-    $('.next').on('click', function () {
-        page++;
-        loadData(page);
-    });
 
-    
-    loadData(page);
-
-
-    
 
     $('.send__data').click(function() {
 
